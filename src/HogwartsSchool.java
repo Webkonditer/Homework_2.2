badae5dbbb0d89ea0522bed9e93b87efcdaf4568
6564747347456845 <webkonditer@yandex.ru>
2022-06-09 23:21:51 +0700

    Промежуточный коммит

---
+++ src/HogwartsSchool.java
@@ -0,0 +1,45 @@
+public class HogwartsSchool {
+
+    private String name;
+    private int powerOfMagic;
+    private int distanceOfTransgression;
+
+    public HogwartsSchool(String name, int powerOfMagic, int distanceOfTransgression) {
+        this.name = name;
+        this.powerOfMagic = powerOfMagic;
+        this.distanceOfTransgression = distanceOfTransgression;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPowerOfMagic() {
+        return powerOfMagic;
+    }
+
+    public void setPowerOfMagic(int powerOfMagic) {
+        this.powerOfMagic = powerOfMagic;
+    }
+
+    public int getDistanceOfTransgression() {
+        return distanceOfTransgression;
+    }
+
+    public void setDistanceOfTransgression(int distanceOfTransgression) {
+        this.distanceOfTransgression = distanceOfTransgression;
+    }
+
+    @Override
+    public String toString() {
+        return "HogwartsSchool{" +
+                "name='" + name + '\'' +
+                ", powerOfMagic=" + powerOfMagic +
+                ", distanceOfTransgression=" + distanceOfTransgression +
+                '}';
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Gryffindor gryffindor = new Gryffindor("Гарри Поттер", 67, 97, 54, 56, 23);
+
+        System.out.println(gryffindor);
     }
 }
